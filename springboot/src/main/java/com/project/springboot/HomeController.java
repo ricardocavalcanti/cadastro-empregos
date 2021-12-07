584c9859fdedcd4c705b986c148f7d081280842e
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-12-07 12:13:38 -0300

    Adcionando nota sobre ferramentas usadas

--- springboot/src/main/java/com/project/springboot/HomeController.java
+++ springboot/src/main/java/com/project/springboot/HomeController.java
@@ -1,7 +1,14 @@
 package com.project.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    // Significa que iremos rodar em localhost:8080
+    @RequestMapping("/")
+    public String homeApp(){
+        return "index";
+    }
 }
