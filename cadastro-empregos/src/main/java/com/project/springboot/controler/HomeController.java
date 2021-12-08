39335e804ebffb765753f77eab13c36f2fca5318
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-12-07 22:14:49 -0300

    Implementando login

--- cadastro-empregos/src/main/java/com/project/springboot/controler/HomeController.java
+++ cadastro-empregos/src/main/java/com/project/springboot/controler/HomeController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
     // Significa que iremos rodar em localhost:8080
     @RequestMapping("/")
     public String homeApp(Model model){
         model.addAttribute("mensagem", "Sistema de cadastro de empregos");
         return "index";
     }
-
-
 }
--- cadastro-empregos/src/main/java/com/project/springboot/security/SecurityConfiguration.java
+++ cadastro-empregos/src/main/java/com/project/springboot/security/SecurityConfiguration.java
@@ -19,7 +19,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected  void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+        // Informando pagina de login personalizada
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
     }
 
     @Override
