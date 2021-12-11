e9160cf2469a7e960478b15609c2a3b70247ef77
ricardo.cavalcanti <ricardocavalcanti@outlook.com>
2021-12-10 22:33:17 -0300

    Implementando fragment template

--- cadastro-empregos/src/main/java/com/project/springboot/controler/HomeController.java
+++ cadastro-empregos/src/main/java/com/project/springboot/controler/HomeController.java
@@ -6,24 +6,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping("/login")
-    public String login(){
-        return "login";
-    }
-
-    // Significa que iremos rodar em localhost:8080
+	
+	// Significa que iremos rodar em localhost:8080
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("mensagem", "Sistema de cadastro de empregos");
         return "index";
     }
 
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }   
+
     @RequestMapping("/admin")
     public String admin(){
         return "admin";
     }
 
+    @RequestMapping("/blog")
+    public String blog(){
+        return "blog";
+    }
+    
+    @RequestMapping("/contato")
+    public String contato(){
+        return "contato";
+    }
+    
     @RequestMapping("/secure")
     public String secure(){
         return "seguranca";
